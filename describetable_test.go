b7fa444441c982845e4d18cc82b080911ce655ec
Jason Underhill <jason@junderhill.com>
2023-03-27 17:14:45 +0100

    Testing describe table to debug error

--- describetable_test.go
+++ describetable_test.go
@@ -2,6 +2,10 @@ package dynamov2
 
 import (
 	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
 func TestDescribeTable(t *testing.T) {
@@ -21,5 +25,53 @@ func TestDescribeTable(t *testing.T) {
 	}
 	if desc.HashKey != "UserID" || desc.RangeKey != "Time" {
 		t.Error("bad keys:", desc.HashKey, desc.RangeKey)
+	}
+}
+
+func TestDescribeTableDescription(t *testing.T) {
+	tableDescription := &types.TableDescription{
+		AttributeDefinitions: []types.AttributeDefinition{
+			{
+				AttributeName: aws.String("PK"),
+				AttributeType: "S",
+			},
+			{
+				AttributeName: aws.String("SK"),
+				AttributeType: "S",
+			},
+		},
+		TableName: aws.String("test"),
+		KeySchema: []types.KeySchemaElement{
+			{
+				AttributeName: aws.String("PK"),
+				KeyType:       "HASH",
+			},
+			{
+				AttributeName: aws.String("SK"),
+				KeyType:       "RANGE",
+			},
+		},
+		TableStatus:      "ACTIVE",
+		CreationDateTime: aws.Time(time.Now()),
+		ItemCount:        1001,
+		TableSizeBytes:   987654321,
+	}
+
+	desc := newDescription(tableDescription)
+
+	if desc.Name != "test" {
+		t.Error("wrong name:", desc.Name, "≠", "test")
+	}
+
+	if desc.HashKey != "PK" || desc.RangeKey != "SK" {
+		t.Error("bad keys:", desc.HashKey, desc.RangeKey)
+	}
+
+	if desc.Items != 1001 {
+		t.Error("bad item count:", desc.Items, "≠", 1001)
+	}
+
+	if desc.Size != 987654321 {
+		t.Error("bad size:", desc.Size, "≠", 987654321)
 	}
 }
